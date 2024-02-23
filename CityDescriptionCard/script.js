057a47814320aa7643c261dff75be3cdaf37e279
STiwari <71030367+Stiwari28@users.noreply.github.com>
2024-02-23 21:02:39 +0530

    .

--- CityDescriptionCard/script.js
+++ CityDescriptionCard/script.js
@@ -8,20 +8,17 @@ fetch('https://api.npoint.io/7bbd3a59c401f616bb89/places/')
     .then(data => {
         console.log(data);
         const CityCard = document.getElementById('cards_section');
-
-        // Iterate over each place and create a new <div> element for each place
+        
         data.forEach(place => {
             const card = document.createElement('div');
-            card.classList.add('card'); // Add a class to style the card if needed
-
-            // Set the HTML content for the card
+            card.classList.add('card'); 
+            
             card.innerHTML = `
                 <h2>${place.name}</h2>
                 <img src="${place.image}" alt="${place.name}">
                 <p>${place.info}</p>
             `;
-
-            // Append the card to the container
+            
             CityCard.appendChild(card);
         });
     })
